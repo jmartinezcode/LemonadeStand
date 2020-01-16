cd5035ca85456fb8ca867e0ad4f06dc5140eac67
Jacob Martinez <jmartinez.data@gmail.com>
2020-01-16 15:41:44 -0600

    Added namesList to Customer

--- Lemonade/Customer.cs
+++ Lemonade/Customer.cs
@@ -11,7 +11,25 @@ namespace Lemonade
         public string name;
         public Customer()
         {
+            List<string> namesList = new List<string>() { "Eva", "Lorine", "Carolee", "Juan", "Parthenia", "Cory",
+                                                          "Dung", "Larisa", "Cyndi", "Rubye", "Aja", "Jerrold",
+                                                          "Johnette", "April", "Criselda", "Tressa", "Theressa", 
+                                                          "Isidro", "Renata", "Danyell", "Perry", "Fe", "Jeremy", 
+                                                          "Dionne", "Dessie",  "Toby", "Christiana", "Stanton", "Leona",
+                                                          "Barabara", "Buddy", "Lynn", "Freddy", "Numbers", "Mae", 
+                                                          "Isela", "Macy", "Alessandra", "Sondra", "Ashlyn", "Jacinda",
+                                                          "Erick", "Maryanna", "Thora", "Junita", "Lizeth", "Alena",
+                                                          "Mandy", "Lisbeth", "Wendi" };
+        }
+        // TO DO
+        // Implement a price factor, weather and heat factor
+        // The price of product as well as weather/temperature should affect demand, 
+        // so that if the price is too high, sales will decrease, or if the price is too low, 
+        // sales will increase, etc.
 
+        public void ProbabilityToPurchase()
+        {
+            
         }
     }
 }
