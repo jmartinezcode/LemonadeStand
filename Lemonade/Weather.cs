66f9b135a97abf257e9f389375d2ad4f80ec96eb
Jacob Martinez <jmartinez.data@gmail.com>
2020-01-16 13:29:21 -0600

    Added methods to the Weather class

--- Lemonade/Weather.cs
+++ Lemonade/Weather.cs
@@ -14,7 +14,22 @@ namespace Lemonade
         public string predictedForecast;
         public Weather()
         {
-            
+            weatherConditions = new List<string>() { "Sunny", "Overcast", "Hazy", "Rain" };
         }
+
+        public void SetTemperature()
+        {
+            // Set Temperature randomly between 50 and 100 degrees
+        }
+        public void SetCondition()
+        {
+            // Randomly pull from weatherConditions List
+        }
+        public void SetForecast()
+        {
+            // Takes output from SetCondition and SetTemperature to output predictedForecast
+        }
+
+
     }
 }
