40d8db81189cc14c80604a3f3ef7793703efa780
Jacob Martinez <jmartinez.data@gmail.com>
2020-01-16 13:41:54 -0600

    Randomly set temperature, determine condition and output string of the two

--- Lemonade/Weather.cs
+++ Lemonade/Weather.cs
@@ -12,22 +12,30 @@ namespace Lemonade
         public int temperature;
         List<string> weatherConditions;
         public string predictedForecast;
+        Random random;
         public Weather()
         {
+            //Random random = new Random();
             weatherConditions = new List<string>() { "Sunny", "Overcast", "Hazy", "Rain" };
+            SetTemperature();
+            SetCondition();
+            SetForecast();
         }
 
         public void SetTemperature()
         {
             // Set Temperature randomly between 50 and 100 degrees
+            temperature = random.Next(50, 101);
         }
         public void SetCondition()
         {
             // Randomly pull from weatherConditions List
+            condition = weatherConditions[random.Next(weatherConditions.Count)];
         }
         public void SetForecast()
         {
             // Takes output from SetCondition and SetTemperature to output predictedForecast
+            predictedForecast = temperature + condition;
         }
 
 
