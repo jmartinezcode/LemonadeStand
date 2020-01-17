edd9b6ee135d911fa00ddb0b56596ac19ab6d3b8
Jacob Martinez <jmartinez.data@gmail.com>
2020-01-17 08:01:52 -0600

    Instantiated lists in the Inventory constructor

--- Lemonade/Inventory.cs
+++ Lemonade/Inventory.cs
@@ -9,12 +9,15 @@ namespace Lemonade
     class Inventory
     {
         public List<Lemon> lemons;
+        public List<SugarCube> sugarCubes;
         public List<IceCube> iceCubes;
         public List<Cup> cups;
-        public List<SugarCube> sugarCubes;
         public Inventory()
         {
             lemons = new List<Lemon>();
+            sugarCubes = new List<SugarCube>();
+            iceCubes = new List<IceCube>();
+            cups = new List<Cup>();
         }
     }
 }
