cba3aef4480e0da9275482984543a1f54b10c68d
Jacob Martinez <jmartinez.data@gmail.com>
2020-01-17 11:13:03 -0600

    Removed testing code from Program

--- Lemonade/Program.cs
+++ Lemonade/Program.cs
@@ -10,13 +10,7 @@ namespace Lemonade
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            for (int i = 0; i < 20; i++)
-            {
-                double pricePerIceCube = random.Next(2, 4) / 100.0;
-                Console.WriteLine(pricePerIceCube);
-            }
-            Console.ReadLine();
+
         }
     }
 }
