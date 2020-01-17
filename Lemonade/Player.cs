82979ab2ec9d0754fcbef5305e0450b728e757ed
Zach Mommaerts <zmommaerts@gmail.com>
2020-01-17 11:25:23 -0600

    added wallet access modifiers and set wallet starting amount to 0

--- Lemonade/Player.cs
+++ Lemonade/Player.cs
@@ -43,6 +43,14 @@ namespace Lemonade
                     {
                         inventory.iceCubes.Remove(new IceCube());
                     }
+                    if(inventory.cups.Count < 12)
+                    {
+                        pitcher.cupsLeftInPitcher = inventory.cups.Count;
+                    }
+                    else
+                    {
+                        pitcher.cupsLeftInPitcher = 12;
+                    }
                 }
                 else
                 {
@@ -52,7 +60,7 @@ namespace Lemonade
         }
         public void SellCup()
         {
-
+            
         }
     }
 }
